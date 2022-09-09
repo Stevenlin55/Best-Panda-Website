447cbf7307a885733ee9c8bed0fac71ac866b1e5
Stevenlin55 <70299036+Stevenlin55@users.noreply.github.com>
2022-09-08 20:26:17 -0500

    changed hours for lunch menu

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/Header.js';
 import Home from './components/Home';
 import Menu from './components/Menu';
-import FAQ from './components/FAQ';
 import Footer from './components/Footer'
 import './styles.css'
 import TagManager from 'react-gtm-module'
@@ -25,7 +24,6 @@ export default class App extends Component {
             <div className="container body">
               <Route path="/" exact component={Home} />
               <Route path="/menu" exact component={Menu}  />
-              <Route path="/faq" exact component={FAQ} />  
             </div>
             <Footer />
           </BrowserRouter>
